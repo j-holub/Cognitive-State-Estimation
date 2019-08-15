458179c3bc63a411dbdb150e42344cbe6d681b17
Jan Holub <Jan-Holub@gmx.net>
2019-08-15 17:03:58 +0200

    Added a Statistics class to compute different metrics on the data

--- lib/dataprocessing/__init__.py
+++ lib/dataprocessing/__init__.py
@@ -4,21 +4,25 @@ This package offers various classes and functions to process the data gathered
 from the experiments.
 
 Classes:
-    ExperimentData
-        Handles the JSON file created by the n-back experiment implemented in
-        JsPsych. Processes the data and offers various method to access the
-        relevant information
-    VideoHandler
-        Handles the video recorded during the experiment and is able to extract
-        the frames needed for the data
     DataHandler
         Handles the processed data and stores it in the right format. It groups
         the frames into chunks of windowsize size and manages labels for those
         chunks
+    ExperimentData
+        Handles the JSON file created by the n-back experiment implemented in
+        JsPsych. Processes the data and offers various method to access the
+        relevant information
+    Statistics
+        Computes various statistics about the experiment data, such as average
+        score and so on
+    VideoHandler
+        Handles the video recorded during the experiment and is able to extract
+        the frames needed for the data
 """
 
-from .experimentdata import ExperimentData
-from .video          import VideoHandler
 from .datahandler    import DataHandler
+from .experimentdata import ExperimentData
+from .statistics     import Statistics
+from .video          import VideoHandler
 
 from .util import *
