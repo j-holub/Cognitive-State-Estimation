d908c7792d1003c11d08e91a70c864d0fc7ba700
Jan Holub <Jan-Holub@gmx.net>
2019-09-08 17:06:53 +0200

    Added the opencv caffe model files to the setup.sh script
    Now they can be downloaded and are not included in the repository anymore

--- setup.sh
+++ setup.sh
@@ -22,10 +22,22 @@ pip3 install ./openface-master
 # get the dlib landmarks file for the facial landmark detection
 cd ${SCRIPT_DIR}
 mkdir -p resources
+
+# open face landmark detector model
 wget \
 	 http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 \
 	 -O resources/shape_predictor_68_face_landmarks.dat.bz2
 bunzip2 resources/shape_predictor_68_face_landmarks.dat.bz2
+
+# caffe model for opencv face detection
+wget \
+	https://github.com/opencv/opencv_3rdparty/raw/19512576c112aa2c7b6328cb0e8d589a4a90a26d/res10_300x300_ssd_iter_140000_fp16.caffemodel \
+	-O resources/res10_300x300_ssd_iter_140000_fp16.caffemodel
+
+wget \
+	https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt \
+	-O resources/deploy.prototxt
+
 
 # remove the unneeded data
 rm -r openface-master master.zip
