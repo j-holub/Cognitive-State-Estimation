8a39b6b19b3e2559649b56917802bf3b80d29f00
Jan Holub <Jan-Holub@gmx.net>
2019-09-10 16:17:28 +0200

    Added a DataHandler class for the deep learning part
    It stores the data for deep learning. Shuffles it, splits it into test and train set and also normalizes it. It also transforms the labels into one hot represantation

---
+++ lib/deeplearning/__init__.py
@@ -0,0 +1,12 @@
+"""Deep Learning Module
+
+This module offers various neural network architectures and utility classes for
+deep learning
+
+Classes:
+    DataHandler
+        This class manages training data for a neural network. It shuffles the data,
+        splits it into train and test dataset and normalizes the data
+"""
+
+from .datahandler import DataHandler
---
+++ lib/deeplearning/datahandler.py
@@ -0,0 +1,78 @@
+import numpy as np
+import keras.utils
+
+class DataHandler:
+    """Class to store and handle training data for neural networks
+
+    It normalizes the data, shuffles it and splits it into a test and a training
+    set. It also transforms the labels into one hot representation.
+
+    Methods:
+        train_data()
+            returns features and labels for the train set
+        test_data()
+            returns features and labels for the test set
+    """
+
+    def __init__(self, features_file: str, labels_file: str, val_split: float):
+        """
+        Parameters:
+            features_file (str):
+                path to the .npy file containing the features
+            labels_file (str):
+                path to the .npy file containing the labels
+            val_split (float):
+                ratio to split the data into train and test set. Must be in the
+                interval (0,1)
+        """
+
+        assert val_split>0 and val_split<1
+
+        # load the data
+        self.__data = np.load(features_file, mmap_mode='r+')
+        # load the labels and transform them to 1 hot
+        self.__labels = np.load(labels_file)
+        self.__labels = keras.utils.to_categorical(self.__labels)[...,1:]
+
+        assert self.__data.shape[0] == self.__labels.shape[0]
+
+        # transform the data if needed
+        # if the dimension is (amount, window, crop crop) transform
+        # it to (amount, window, crop, crop, 1)
+        if(len(self.__data.shape)==4):
+            self.__data = np.reshape(self.__data, (*self.__data.shape, 1))
+
+        # shuffle the data
+        permutation = np.random.permutation(self.__data.shape[0])
+        self.__data   = np.take(self.__data, permutation, axis=0)
+        self.__labels = np.take(self.__labels, permutation, axis=0)
+
+        # compute the index to divide the data into training and validation set
+        self.__split_index = int(self.__data.shape[0] * val_split)
+
+        # normalize the data
+        self.__data = (self.__data - np.mean(self.__data)) / np.std(self.__data)
+
+
+    def train_data(self):
+        """Returns the train dataset
+
+        Returns:
+            (np.ndarray, np.ndarray): train features and train labels as numpy
+                arrays of the according dimensions
+
+        """
+        
+        return self.__data[:self.__split_index], self.__labels[:self.__split_index]
+
+
+    def test_data(self):
+        """Returns the test dataset
+
+        Returns:
+            (np.ndarray, np.ndarray): test features and test labels as numpy
+                arrays of the according dimensions
+
+        """
+
+        return self.__data[self.__split_index:], self.__labels[self.__split_index:]
