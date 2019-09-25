b5922b8fbea79fb1882f59e423bcc5c68626eae4
Jan Holub <Jan-Holub@gmx.net>
2019-09-25 14:12:28 +0200

    Added the windowsize as an input paramenter for the CLitW network

--- lib/deeplearning/cognitiveloadinthewild.py
+++ lib/deeplearning/cognitiveloadinthewild.py
@@ -1,9 +1,9 @@
 from keras.models import Sequential
 from keras.layers import Conv3D
 from keras.layers import MaxPooling3D, GlobalMaxPool3D
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 
-def CLitW_network():
+def CLitW_network(windowsize: int = 60):
     """The neural network as proposed in 'Cognitive Load in the Wild'
 
     This CNN was proposed for 0-back / 1-back / 2-back classification using
@@ -11,13 +11,18 @@ def CLitW_network():
 
     This network has 5 neurons on the output layer for 5 class classification
 
+    Parameters:
+        windowsize (int):
+            width of one chunk that is input to the network
+            default: 60
+
     Returns:
         keras model: the model object storing the information about the network
     """
 
     model = Sequential()
 
-    model.add(Conv3D(128, (3,3,3), activation='relu', input_shape=(60,64,64,1)))
+    model.add(Conv3D(128, (3,3,3), activation='relu', input_shape=(windowsize,64,64,1)))
 
     model.add(Conv3D(128, (3,3,3), activation='relu'))
 
