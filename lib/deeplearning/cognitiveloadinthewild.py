1ad7ce59098986e08eba8036a2897f198c3e794c
Jan Holub <Jan-Holub@gmx.net>
2019-11-19 21:13:45 +0100

    Added padding to keep the image dimension for clitw
    In the original paper by Fridman et al it was stated, that conv layers have a padding to keep the image dimensions

--- lib/deeplearning/cognitiveloadinthewild.py
+++ lib/deeplearning/cognitiveloadinthewild.py
@@ -22,23 +22,23 @@ def CLitW_network(windowsize: int = 60):
 
     model = Sequential()
 
-    model.add(Conv3D(128, (3,3,3), activation='relu', input_shape=(windowsize,64,64,1)))
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same', input_shape=(windowsize,64,64,1)))
 
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
-
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2,2,2)))
-
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
-
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
 
     model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2,2,2)))
 
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
 
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
 
-    model.add(Conv3D(128, (3,3,3), activation='relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2,2,2)))
+
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
+
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
+
+    model.add(Conv3D(128, (3,3,3), activation='relu', padding='same'))
 
     model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2,2,2)))
 
